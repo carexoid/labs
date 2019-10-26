bbb47f6e9c9397ec1177d48ecd03610405425f74
carexoid <osokadania@gmail.com>
2019-10-26 19:37:03 +0300

    Task 2: fixed the way of counting result for string input ( to a/b, where a - number of digits, b - length of string)

--- 2_task/main.cpp
+++ 2_task/main.cpp
@@ -33,20 +33,20 @@ double funcF<double> (const double& input) {
 
 template<>
 double funcF<std::string>( const std::string& input){
-    double div = 1;
+    double digCounter = 0;
     for (const auto& i: input){
-        try{
-            if( i - '0' == 0)
-                throw "Division by 0 in fuction with string in input!";
-        }
-        catch (char const*str){
-            std::cout << '\n' << str << '\n';
-            return 0;
-        }
-        if (i - '0' > 0 && i - '0' <= 9)
-            div /= (i - '0');
+        if (i - '0' >= 0 && i - '0' <= 9)
+            digCounter++;
     }
-    return div*div;
+    try{
+        if(!input.length())
+            throw "Division by 0 in fuction with string in input!";
+    }
+    catch (char const*str){
+        std::cout << '\n' << str << '\n';
+        return 0;
+    }
+    return ((double) digCounter / input.length()) *  ((double) digCounter / input.length());
 }
 
 template<typename T, typename  V>
@@ -57,18 +57,19 @@ double funcF(const std::pair<T, V>& input) {
 int main() {
     int intTest1 = 10, intTest2 = -1;
     double douTest1 = 10, douTest2 = 1.5;
-    std::string strTest = "aret1uy2hkov5f";
+    bool boolTest = true;
+    std::string strTest = "aretuyhkovf";
     std::pair<int,double> pairIntDouTest1 = {intTest2,douTest2};
     std::pair<std::string,double> pairIntDouTest2 = {strTest,douTest2};
     std::cout << "\nTesting (int) 10 as parametr: " << funcF(intTest1);
     std::cout << "\nTesting (double) 10 as parametr: " << funcF(douTest1);
     std::cout << "\nTesting (int) -1 as parametr: " << funcF(intTest2);
     std::cout << "\nTesting (int) -10 as parametr: " << funcF(10*intTest2);
-    std::cout << "\nTesting (string) aret1uy2hkov5f as parametr: " << funcF(strTest);
-    std::cout << "\nTesting (string) 1aret1uy2hkov5f0 as parametr: " << funcF(strTest + "0");
+    std::cout << "\nTesting (string) aretuyhkovf as parametr: " << funcF(strTest);
+    std::cout << "\nTesting (string) aretuyhkovfw34e4r as parametr: " << funcF(strTest + "w34e4r");
     std::cout << "\nTesting (pair) { (int) -1, (double) 1.5 } as parametr: " << funcF(pairIntDouTest1);
     std::cout << "\nTesting (pair) { (string) aret1uy2hkov5f, (double) 1.5 } as parametr: " << funcF(pairIntDouTest2);
-
+    std::cout << "\nTesting (bool) true as parametr: " << funcF(boolTest);
 
 
     return 0;
