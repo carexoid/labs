77825734b3e4caaddc4e52d38f5a51e1b71fb165
carexoid <osokadania@gmail.com>
2019-10-26 12:18:09 +0300

    added projects for 2 and 3 tasks

---
+++ 3_task/cmake-build-debug/CMakeFiles/3_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3_task"
+  "3_task.pdb"
+  "CMakeFiles/3_task.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
