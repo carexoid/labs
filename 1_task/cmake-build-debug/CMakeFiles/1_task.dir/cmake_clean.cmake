a7210e6501623eea074f979326587756ca48eb53
carexoid <osokadania@gmail.com>
2019-10-26 12:11:49 +0300

    adding 1 task project to repo.

---
+++ 1_task/cmake-build-debug/CMakeFiles/1_task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1_task"
+  "1_task.pdb"
+  "CMakeFiles/1_task.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
